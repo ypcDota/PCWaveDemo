0cb289f9e79eab2ddf102f0ca53efafa9cdf1e8d
PCweibo <985348196@qq.com>
2017-03-20 13:31:44 +0800

    初始化项目

---
+++ PCWaveDemo/PCWaveDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PCWaveDemo
+//
+//  Created by ypc on 17/3/20.
+//  Copyright © 2017年 com.ypc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PCWaveDemo/PCWaveDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PCWaveDemo
+//
+//  Created by ypc on 17/3/20.
+//  Copyright © 2017年 com.ypc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
